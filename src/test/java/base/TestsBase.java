59c3685941ff3e537fcb5e417fed0b423959e8f1
Ivan Belik <belik.ivan89@gmail.com>
2018-09-04 21:46:09 +0300

    Закоментирован вебрайвер Chrome, добавлен веб драйвер Firefox

--- src/test/java/base/TestsBase.java
+++ src/test/java/base/TestsBase.java
@@ -2,25 +2,27 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
-import java.util.concurrent.TimeUnit;
 
 public class TestsBase {
     protected static WebDriver driver;
 
     @BeforeClass
     public void SetupDrivers() {
-        System.setProperty("webdriver.chrome.driver", "E:/InstallProgram/drivers/chromedriver/chromedriver.exe");
-        driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver", "E:/InstallProgram/drivers/chromedriver/chromedriver.exe");
+//        driver = new ChromeDriver();
+
+        System.setProperty("webdriver.gecko.driver", "E:/InstallProgram/drivers/geckodriver/geckodriver.exe");
+        driver = new FirefoxDriver();
     }
 
     @BeforeMethod
     public void SetupConfig() {
-        driver.get("https://ffcdev.fundraisingforacause.com/");
+        driver.get("https://ffcstage.fundraisingforacause.com/");
         driver.manage().window().maximize();
     }
 
